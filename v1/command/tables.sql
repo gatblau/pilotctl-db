0f51485fb7cd9a6c2b48cf90da1509b897564012
gatblau <onix@gatblau.org>
2022-04-06 19:40:26 +0100

    remove mac_address unique constraint to allow for re-registration of decom host

--- v1/command/tables.sql
+++ v1/command/tables.sql
@@ -61,8 +61,7 @@ $$
                 -- the date the host was decommissioned, null if the host is active
                 decom_date   TIMESTAMP(6) WITH TIME ZONE,
                 CONSTRAINT host_id_pk PRIMARY KEY (id),
-                CONSTRAINT host_key_uc UNIQUE (host_uuid),
-                CONSTRAINT mac_address_uc UNIQUE (mac_address)
+                CONSTRAINT host_key_uc UNIQUE (host_uuid)
             ) WITH (OIDS = FALSE)
               TABLESPACE pg_default;
 
